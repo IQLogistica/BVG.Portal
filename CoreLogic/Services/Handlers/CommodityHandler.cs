b1db314a5a9316d7c3359753ec50efb5f1b596b3
Michael Coulentianos <Michael.C@iqlogistica.com>
2024-01-20 09:14:43 +0200

    first commit

---
+++ CoreLogic/Services/Handlers/CommodityHandler.cs
@@ -0,0 +1,20 @@
+﻿using CoreLogic.Contracts;
+using CoreLogic.Services.Commands;
+using MediatR;
+
+namespace CoreLogic.Services.Handlers;
+
+public class CommodityHandler : IRequestHandler<CommodityPriceFeedCommand, Task>
+{
+    private readonly ICommodityHistoryRepository CommodityHistoryRepository;
+    public CommodityHandler(ICommodityHistoryRepository commodityHistoryRepository)
+    {
+        CommodityHistoryRepository = commodityHistoryRepository;
+    }
+
+    public async Task<Task> Handle(CommodityPriceFeedCommand request, CancellationToken cancellationToken)
+    {
+        CommodityHistoryRepository.Insert(request.Commodities.commodityData);
+        return Task.CompletedTask;
+    }
+}
